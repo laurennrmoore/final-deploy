bb97b7b496740cd883f7c31feb52a88817d82030
lmoore <lmooreintech2021@gmail.com>
2021-06-02 16:38:07 -0400

    Wednesdaypush

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
 import React, { Component } from 'react'
+import DemoCarousel from './Components/carousel'
+import Navbar from './Components/Navbar'
+import { BrowserRouter } from 'react-router-dom'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-      
+      <BrowserRouter>
+      <Navbar />
+      <DemoCarousel/>
+      </BrowserRouter>
         
       </div>
     )
