7fd7ef815eb062f09121044eff5fd04a488019e7
Mona <emonearth915@gmail.com>
2021-06-04 16:33:20 -0400

    please work

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import '../About.css';
+import './Footer.css';
 
-import React from "react";
 import Cincinnati from "../img/footer-img/KrogerLogo.png";
 import CBell from "../img/footer-img/CincinnatiBell-logo.png";
 import FifthThird from "../img/footer-img/fifththirdbanklogo.png";
@@ -15,39 +14,46 @@ import Skylinechili from "../img/footer-img/skyline_logo.png";
 // import linkedin from "../img/footer-img/linkedin-log.png";
 import '../../node_modules/font-awesome/css/font-awesome.min.css'; 
 import { TiSocialLinkedin } from "react-icons/ti";
+import { MdFavoritehearts } from "react-icons/md";
 const Footer = () => (
-   <div className="container">
-       <div className="container" id="sponsors">
-           <img alt='' src= { Cincinnati } /> 
-           <img alt='' src= { CBell } /> 
-           <img alt='' src= { FifthThird } /> 
-           <img alt='' src= { Kao } />
-           <img alt='' src= { Macy } />  
-           <img alt='' src= { PandG } /> 
-           <img alt='' src= { UC } /> 
-           <img alt='' src= { CincinnatiRed } /> 
-           <img alt='' src= { cincyZoo } />
-           <img alt='' src= { Skylinechili } />
-
-        <h5 id="">CREATORS</h5>
-
-        <p>Lauren Moore <br/>
-        <a id="Lauren" href="https://www.linkedin.com/in/lauren-m-a233b321/"><TiSocialLinkedin size="3em"></TiSocialLinkedin></a></p> 
-
-        <p>Marquis Jarman<br/>
-        <a id="Marquis" href="https://www.linkedin.com/in/marquis-jarman-9a1113210//"><TiSocialLinkedin size="3em" ></TiSocialLinkedin></a></p> 
-        
-        <p>Mona Jira<br/>
-        <a id="Mona" href="https://www.linkedin.com/in/mona-jiravirotekul-26347768/"><TiSocialLinkedin size="3em" ></TiSocialLinkedin></a></p> 
-        
-        
-        <p id="footer-text">We devoted our times and hearts to create the content of our beloved city.
-         Hope you guys find it is useful infomation when you travel in our hometown. 
-         Let us know</p>
-      
-      </div>
+   <div className="footer-container">
+   <div className="container" id="sponsor-container">
+           <img className="sponsor-logo" alt='' src= { Cincinnati } /> 
+           <img className="sponsor-logo" alt='' src= { CBell } /> 
+           <img className="sponsor-logo" alt='' src= { FifthThird } /> 
+           <img className="sponsor-logo" alt='' src= { Kao } />
+           <img className="sponsor-logo" alt='' src= { Macy } />  
+           <img className="sponsor-logo" alt='' src= { PandG } /> 
+           <img className="sponsor-logo" alt='' src= { UC } /> 
+           <img className="sponsor-logo" alt='' src= { CincinnatiRed } /> 
+           <img className="sponsor-logo" alt='' src= { cincyZoo } />
+           <img className="sponsor-logo" alt='' src= { Skylinechili } />
        
+</div>
+        <h2 id="creators">CREATORS</h2>
+    <div className="grid-container">
+      <div className='name1'>
+            <h4>Lauren Moore </h4>
+            <p><a id="Lauren" href="https://www.linkedin.com/in/lauren-m-a233b321/"><TiSocialLinkedin color="blue" size="3em"></TiSocialLinkedin></a></p> 
+      </div> 
+      
+      <div className='name2'>
+            <h4>Marquis Jarman</h4>
+            <p><a id="Marquis" href="https://www.linkedin.com/in/marquis-jarman-9a1113210//"><TiSocialLinkedin color="blue" size="3em" ></TiSocialLinkedin></a></p> 
+      </div>  
+
+      <div className='name3'>     
+            <h4>Mona Jira</h4>
+            <p><a id="Mona" href="https://www.linkedin.com/in/mona-jiravirotekul-26347768/"><TiSocialLinkedin color="blue" size="3em" ></TiSocialLinkedin></a></p> 
     </div>
+      </div> 
+            <p id="footer-text">We devoted our times and <a href="MdFavoritehearts"></a>heart to create the content of our beloved city.
+             Hope you guys find it is useful infomation when you travel in our hometown. 
+             Let us know. </p>
+      
+     
+      </div>
+  
   );
   
 
