aaa96e5829db307229649bc2d8676d04ef58562b
Marquis <Marquis.jayshaun.jarman@gmail.com>
2021-06-07 09:40:30 -0400

    puuling

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+
 
 app.use(cors());
 app.use(express.json());
